74c614ae26682ce1b3b5a3df163d4905cc6574c3
N4d3sh <vusezet@gmail.com>
2025-03-17 21:28:16 +0300

    FirstCommit

---
+++ TestMultipleLogger.py
@@ -0,0 +1,19 @@
+import LoggingModule
+import threading
+
+
+if __name__ == "__main__":
+    print("Start logging from multiple threads")
+    logger = LoggingModule.LoggerFactory.create_logger('my_logger', log_format='text', log_file='app.log')
+    thread_safe_logger = LoggingModule.ThreadSafeLogger(logger)
+
+    # Создаем несколько потоков
+    threads = []
+    for i in range(5):  # Создадим 5 потоков
+        thread = threading.Thread(target=LoggingModule.log_messages, args=(i, thread_safe_logger))
+        threads.append(thread)
+        thread.start()
+
+    # Ожидаем завершения всех потоков
+    for thread in threads:
+        thread.join()
